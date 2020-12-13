8edacadeec72471a835e0dc74f8f414b45cd95c5
EG5924 <67492348+egilbertsen@users.noreply.github.com>
2020-12-12 18:10:36 -0800

    retrying react

---
+++ Google-Books-App/google-books/src/pages/NoMatch.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NoMatch() {
+    return (
+        <container fluid>
+            <row>
+                <col>
+                    <jumbotron>
+                        <h1>404 Page Not Found</h1>
+                    </jumbotron>
+                </col>
+            </row>
+        </container>
+    );
+}
+
+export default NoMatch;
