dd9f9f0f14cf0a6f5a9bd452f19fbb298baa3691
EG5924 <67492348+egilbertsen@users.noreply.github.com>
2021-02-17 15:32:17 -0800

    deploying

---
+++ client/build/precache-manifest.0deea4307f3ee82afffc341513d41546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d739e54e4060fcf3993a05ea3dd6a3ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80ac7bb14cf7de7279d0",
+    "url": "/static/css/main.25b6a663.chunk.css"
+  },
+  {
+    "revision": "a645300532088beb6db1",
+    "url": "/static/js/2.07b94edb.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.07b94edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ac7bb14cf7de7279d0",
+    "url": "/static/js/main.f0dc195a.chunk.js"
+  },
+  {
+    "revision": "81df0654baa799a38286",
+    "url": "/static/js/runtime-main.e9d0bf34.js"
+  }
+]);
