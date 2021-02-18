0c336c3ba28e9aae4497e81f71ac2a6a012fb88d
EG5924 <67492348+egilbertsen@users.noreply.github.com>
2021-02-18 12:07:55 -0800

    adding book animation

--- client/src/components/BookSpine/index.js
+++ client/src/components/BookSpine/index.js
@@ -21,31 +21,33 @@ export default class BookSpine extends Component {
 
     render() {
         let isOpenStyle;
-        let bookCover;
+        let bookCoverAnimation;
+
         if (this.state.open) {
+
             isOpenStyle = {
                 margin: "5px",
                 boxShadow: "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px"
             }
 
-            bookCover =
-                <div className="coverBod">
-                    <div className="coverImgHolder">
-                        <img src={this.props.image} className="coverImg" alt="bookcover" />
-                    </div>
-                    <div className="coverText">
-                        <Link to={this.props.link}>
-                            <h4>
-                                {this.props.title} by {this.props.authors}
-                            </h4>
-                        </Link>
-                        <p><span className="desc">Description: </span>{this.props.description} </p>
-                        <DeleteBtn onClick={this.props.handleDelete} />
-                    </div>
-                </div>
+            bookCoverAnimation = {
+                opacity: "1",
+                height: "300px",
+                width: "400px",
+                transition: "all 1s ease"
+            }
+
         } else {
-            isOpenStyle = {boxShadow: "none"}
-            bookCover = <div></div>
+            
+            isOpenStyle = { boxShadow: "none" }
+
+            bookCoverAnimation = {
+                width: "000px",
+                height: "300px",
+                opacity: "0",
+                transition: "all 1s ease"
+
+            }
         }
 
 
@@ -57,7 +59,23 @@ export default class BookSpine extends Component {
                         {this.props.title} by {this.props.authors}
                     </h3>
                 </div>
-                {bookCover}
+                
+                    <div className="coverBod" style={bookCoverAnimation}>
+                        <div className="coverImgHolder">
+                            <img src={this.props.image} className="coverImg" alt="bookcover" />
+                        </div>
+                        <div className="coverText">
+                            <Link to={this.props.link}>
+                                <h4>
+                                    {this.props.title} by {this.props.authors}
+                                </h4>
+                            </Link>
+                            <p><span className="desc">Description: </span>{this.props.description} </p>
+                            <DeleteBtn onClick={this.props.handleDelete} />
+                        </div>
+                    </div>
+                
+
             </div>
         )
     }
